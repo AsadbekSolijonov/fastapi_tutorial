f66b1cc2b758bef9b199781d3f69f0db4110b9bd
SolijonovAsadbek <109437399+AsadbekSolijonov@users.noreply.github.com>
2025-09-16 17:54:07 +0500

    related attributes create

--- db.py
+++ db.py
@@ -1,11 +1,12 @@
 # (SQLAlchemy + Pydantic) -> SQLModel
-from __future__ import annotations
-from sqlmodel import SQLModel, Field, create_engine, Session, select, or_
+# from __future__ import annotations
+from sqlmodel import SQLModel, Field, create_engine, Session, select, or_, Relationship
 
 
 class Team(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
+    heroes: list["Hero"] = Relationship(back_populates='team')
 
 
 class Hero(SQLModel, table=True):
@@ -14,9 +15,24 @@ class Hero(SQLModel, table=True):
     secret_name: str
     age: int | None = None
     team_id: int | None = Field(default=None, foreign_key='team.id')
+    team: Team | None = Relationship(back_populates='heroes')
+
+    # address: "Address" = Relationship(back_populates='hero')
+
+
+# class Address(SQLModel, table=True):
+#     hero_id: int | None = Field(default=None, foreign_key='hero.id', unique=True)
+#     hero: Hero = Relationship(back_populates='address')
+#
+#
+# class HeroTeam(SQLModel, table=True):
+#     hero_id: int = Field(foreign_key='hero.id')
+#     team_id: int = Field(foreign_key='team.id')
 
 
 # SELECT * FROM users Where id=2; DROP table users;  # hujumchi client
+
+
 # Dbeaver(Yengil, Mysql, Sql, Postgres, MongoDb), Pgadmin 4 (Interface)
 
 database_name = "heros.db"
@@ -28,90 +44,54 @@ def create_db():
     SQLModel.metadata.create_all(engine)
 
 
-def creating_hero():
+def create_team_hero():
     with Session(engine) as session:
-        hero_spiderman = Hero(name='Spiderman', secret_name='Piter Parker', age=18)
-        print(f"\tSession ga qo'shilishidan avval:")
-        print(f"\tSpiderman: {hero_spiderman}")
+        team_stars = Team(name='Stars')
+        team_sun = Team(name='Sun')
+        session.add(team_stars)
+        session.add(team_sun)
+        session.commit()
+
+        hero_spiderman = Hero(name='Spiderman', secret_name='Piter', team_id=team_stars.id)
+        hero_hulk = Hero(name='Hulk', secret_name='Bruse', team_id=team_stars.id)
+        hero_amerika = Hero(name='Capitan America', secret_name='Stiff', team_id=team_sun.id)
 
         session.add(hero_spiderman)
-        print(f"\tSession ga qo'shilishidan keyin:")
-        print(f"\tSpiderman: {hero_spiderman}")
-
+        session.add(hero_hulk)
+        session.add(hero_amerika)
         session.commit()
-        print(f"\tCommitga qo'shilishidan keyin:")
-        print(f"\tSpiderman: {hero_spiderman}")
-        print(f"\tSpiderman ID: {hero_spiderman.id}")
-
-        session.refresh(hero_spiderman)
-        print(f"\tRefresh qilinganda keyin:")
-        print(f"\tSpiderman: {hero_spiderman}")
-        print(f"\tSpiderman ID: {hero_spiderman.id}")
 
 
-def read_hero():
+def create_team_hero_with_related_attributes():
     with Session(engine) as session:
-        # AND
-        # statement = select(Hero).where(Hero.name == 'Spiderman').where(Hero.age==18)
-        # statement = select(Hero).where(Hero.name == 'Spiderman', Hero.age == 18)
+        team_ocean = Team(name='Ocean')
+        team_mountain = Team(name='Mountain')
 
-        # OR
-        # statement = select(Hero).where(or_(Hero.name == 'Spiderman', Hero.age == 18))
-
-        # get
-        hero_spiderman = session.get(Hero, 1)
-        # result = session.exec(statement)
-        # hero_spiderman = result.first()
-        print(F"\tHero: {hero_spiderman}")
-
-
-def update_hero():
-    with Session(engine) as session:
-        hero_spiderman = session.get(Hero, 1)
-        print(F"\tHero: {hero_spiderman}")
-        hero_spiderman.age = 25
-        print(f"\tSessionga qo'shilishidan avval:")
-        print(f"\tHero: {hero_spiderman}")
+        hero_spiderman = Hero(name='Kunfu panda', secret_name='Po', team=team_ocean)
+        hero_hulk = Hero(name='Shifu', secret_name='Ustoz', team=team_mountain)
+        hero_amerika = Hero(name='Ugvay', secret_name='Turtle', team=team_ocean)
 
         session.add(hero_spiderman)
-        print(f"\tSessionga qo'shilishidan keyin:")
-        print(f"\tHero: {hero_spiderman}")
-
+        session.add(hero_hulk)
+        session.add(hero_amerika)
         session.commit()
-        print(f"\tCommit qilingandan keyin:")
-        print(f"\tHero: {hero_spiderman}")
-
-        session.refresh(hero_spiderman)
-        print(f"\tRefresh qilingandan keyin:")
-        print(f"\tHero: {hero_spiderman}")
 
 
-def delete_hero():
+def create_hero_team():
     with Session(engine) as session:
-        hero_spiderman = session.get(Hero, 1)
-        print(F"\tObyekt o'chirilishidan avval:")
-        print(F"\tHero: {hero_spiderman}")
-        session.delete(hero_spiderman)
-        print(F"\tObyekt o'chirilishidan keyin:")
-        print(F"\tHero: {hero_spiderman}")
+        hero_sit = Hero(name='Sit', secret_name='Yalqov sit', age=5)
+        hero_mamunt = Hero(name='Mamunt', secret_name='Elephent', age=7)
+
+        team_ice = Team(name='Ice', heroes=[hero_sit, hero_mamunt])
+        session.add(team_ice)
         session.commit()
-        print(F"\tCommit qilingandan keyin:")
-        print(F"\tHero: {hero_spiderman}")
-        hero_spiderman.age = 35
-        session.add(hero_spiderman)
-        session.commit()
-        session.refresh(hero_spiderman)
-        print(hero_spiderman)
-    print(f"\tSessiondan tashqaridagi xolat:")
-    print(F"\tHero: {hero_spiderman}")
 
 
 def main():
     create_db()
-    # creating_hero()
-    # read_hero()
-    # update_hero()
-    # delete_hero()
+    # create_team_hero()
+    # create_team_hero_with_related_attributes()
+    create_hero_team()
 
 
 if __name__ == '__main__':
