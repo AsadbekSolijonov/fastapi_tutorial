9c7efafc31774ab5045c39477accb4fd240e8a7f
SolijonovAsadbek <109437399+AsadbekSolijonov@users.noreply.github.com>
2025-09-18 17:04:22 +0500

    foreign keys

--- db.py
+++ db.py
@@ -1,12 +1,12 @@
 # (SQLAlchemy + Pydantic) -> SQLModel
 # from __future__ import annotations
-from sqlmodel import SQLModel, Field, create_engine, Session, select, or_, Relationship
+from sqlmodel import SQLModel, Field, create_engine, Session, select, or_, Relationship, text
 
 
 class Team(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str
-    heroes: list["Hero"] = Relationship(back_populates='team')
+    heroes: list["Hero"] = Relationship(back_populates='team', cascade_delete=True)
 
 
 class Hero(SQLModel, table=True):
@@ -14,7 +14,7 @@ class Hero(SQLModel, table=True):
     name: str = Field(index=True)
     secret_name: str
     age: int | None = None
-    team_id: int | None = Field(default=None, foreign_key='team.id')
+    team_id: int | None = Field(default=None, foreign_key='team.id', ondelete='CASCADE')
     team: Team | None = Relationship(back_populates='heroes')
 
     # address: "Address" = Relationship(back_populates='hero')
@@ -42,6 +42,8 @@ engine = create_engine(sql_url, echo=True)
 
 def create_db():
     SQLModel.metadata.create_all(engine)
+    with Session(engine) as session:
+        session.execute(text('pragma foreign_keys=on'))  # only for sqlite
 
 
 def create_team_hero():
@@ -87,11 +89,61 @@ def create_hero_team():
         session.commit()
 
 
+def get_heroes():
+    with Session(engine) as session:
+        statement = select(Hero)
+        results = session.exec(statement).all()
+        for result in results:
+            print(result)
+
+
+def get_heroes_limit():
+    with Session(engine) as session:
+        statement = select(Hero).limit(3)
+        results = session.exec(statement)
+        for result in results:
+            print(result)
+
+
+def get_heroes_limit_offset():
+    with Session(engine) as session:
+        statement = select(Hero).limit(3).offset(1)
+        results = session.exec(statement)
+        for result in results:
+            print(result)
+
+
+def update_hero():
+    with Session(engine) as session:
+        statement = select(Hero).where(Hero.name == 'Sit')
+        result = session.exec(statement)
+        hero_sit = result.one()
+        hero_sit.name = 'Sit ice'
+        session.add(hero_sit)
+        session.commit()
+        session.refresh(hero_sit)
+        print(F"Hero sit:", hero_sit)
+
+
+def delete_team():
+    with Session(engine) as session:
+        statement = select(Team).where(Team.name == 'Ice')
+        result = session.exec(statement)
+        team_ice = result.one()
+        session.delete(team_ice)
+        session.commit()
+
+
 def main():
     create_db()
-    # create_team_hero()
-    # create_team_hero_with_related_attributes()
+    create_team_hero()
+    create_team_hero_with_related_attributes()
     create_hero_team()
+    # get_heroes()
+    # get_heroes_limit()
+    # get_heroes_limit_offset()
+    # update_hero()
+    # delete_team()   # orm
 
 
 if __name__ == '__main__':
